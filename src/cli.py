42770f7f2dd349ac92231f6c9f19c455f9de49f2
Vedant Rathore <vedantr1998@gmail.com>
2018-02-15 19:45:18 +0530

    Version 1 done

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,31 @@
 import click
+from janitor import Janitor
+from hurry.filesize import size
+import click_spinner
 
-@click.command()
-@click.option('--text', default="World", help="The greeting you want to see")
-def hello(text):
-  print "Hello {0}!".format(text)
+@click.group()
+def main():
+	pass
+
+@main.command()
+@click.option('--length', '-l',  default=10, help="The number of files you want to see after analysis")
+@click.argument('path', type=click.Path(), default=".")
+def analyse(path, length):
+	click.clear()
+	click.secho('Your top {0} files occupying the most disk space are: '.format(length), fg='white')
+	click.secho('Name'.ljust(30) + 'Size'.ljust(15) + 'Path'.ljust(40) + 'Extension'.ljust(10), fg='white', bold=True)
+	cleaner = Janitor(path)
+	with click_spinner.spinner():
+		files = cleaner.analyse()
+	for index, file in enumerate(files):
+		if index > length:
+			break
+		if len(file['name']) > 28:
+			name = file['name'][:20] + '...'
+		else:
+			name = file['name']
+		if len(file['path']) > 35:
+			path = file['path'][:32] + '...'
+		else:
+			path = file['path']
+		click.secho(name.ljust(30) + str(size(file['size'])).ljust(15) + path.ljust(40) + file['extension'].ljust(10))
